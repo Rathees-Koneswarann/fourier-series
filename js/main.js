9a1777d0339b921d5178b90935c53ba1b8494d6d
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-05 11:57:54 +0530

    Create a moving circle throug big circle

--- js/main.js
+++ js/main.js
@@ -7,19 +7,30 @@ let time = 0;
 
 
 function setup() {
+    // create canvas
   createCanvas(600, 400);
 }
 
+// loop function of p5.js
 function draw() {
-    background(0);
-    translate(200,200);
-    let radius = 50;
-
-    stroke(255);
-    noFill();
-
-    ellipse(0, 0, radius *2);
     
-    time += 0.001;
+    background(0); // set background color of canvas as black
+    translate(200, 200); // bring the drawing poit to this coordinate
+
+    let radius = 50; // set the radius
+
+    stroke(255); // set the border as white
+    noFill(); // remove fill for circle
+    ellipse(0, 0, radius *2); // drw the circle
+
+    let x = radius * cos(time); // get the x coordinates in circle now
+    let y = radius * sin(time); // get the y coordinate in circle now
+
+    fill(255);
+    ellipse(x, y, 8);
+    
+
+    
+    time += 0.01; // increase the time
 
 }
