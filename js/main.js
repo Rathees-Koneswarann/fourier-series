ce3980423275fcf64fbc065677477f09fcdfd37a
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-05 13:16:15 +0530

    Plot fourier series

--- js/main.js
+++ js/main.js
@@ -18,26 +18,35 @@ function draw() {
     background(0); // set background color of canvas as black
     translate(200, 200); // bring the drawing poit to this coordinate
 
-    let radius = 50 * (4 / (1 *PI)); // set the radius
+    let x = 0;
+    let y = 0;    
 
-    stroke(255); // set the border as white
-    noFill(); // remove fill for circle
-    ellipse(0, 0, radius *2); // drw the circle
+    for (let i = 0; i < 5; i++) {
 
-    let x = radius * cos(1 * time); // get the x coordinates in circle now
-    let y = radius * sin(1 * time); // get the y coordinate in circle now
+        let prevx = x;
+        let prevy = y;
+        let n = i * 2 + 1;
+        let radius = 50 * (4 / (n *PI)); // set the radius
+        x += radius * cos(n * time); // get the x coordinates in circle now
+        y += radius * sin(n * time); // get the y coordinate in circle now    
+   
+        stroke(255, 100); // set the border as white
+        noFill(); // remove fill for circle
+        ellipse(prevx, prevy, radius *2); // draw the circle
 
-    wave.unshift(y); // collect y coordinate and add it first in array
-
-    fill(255);
-    ellipse(x, y, 8); // small circle at big circle circumference
-
-    line (0, 0, x, y); // line start from big circle's centre and end in small circles center
-    
+        fill(255);
+        //ellipse(x, y, 5); // small circle at big circle circumference
+        stroke(255);
+        line (prevx, prevy, x, y); // line start from big circle's centre and end in small circles center
+        
+    }
     translate(180,0); // move point
     line(x-180, y, 0, wave[0]) // draw the line from small circle to wave starting point
+    wave.unshift(y); // collect y coordinate and add it first in array
+
     beginShape();
     noFill();
+
     for (let i = 0; i < wave.length; i++) {
         vertex(i, wave[i]); // draw curve
     }
